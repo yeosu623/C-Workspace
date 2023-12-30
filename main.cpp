102fa5af38ba191baf54e79c9b3dd43e6ae0cf6d
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2023-12-31 03:14:51 +0900

    code update.

--- main.cpp
+++ main.cpp
@@ -1,34 +1,13 @@
+#include <cstdio>
+#include <cstdlib>
 #include <iostream>
 using namespace std;
 
-class AAA {
-private:
-	int num;
-public :
-	AAA()
-		: num(0) {
-
-	}
-
-	AAA& CreateInitObj(int n) const {
-		AAA* ptr = new AAA(n);
-		return *ptr;
-	}
-
-	void ShowNum() const {
-		cout << num << endl;
-	}
-private:
-	AAA(int n)
-		: num(n) {
-
-	}
-};
-
 int main() {
-	AAA a1;
-	AAA& a2 = a1.CreateInitObj(10);
-	a2.ShowNum();
+	int* ptr = new int;
+	int& ref = *ptr;
+	ref = 200;
+	cout << ref << endl;
 
 	return 0;
 }
