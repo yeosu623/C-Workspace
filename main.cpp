636788cc77ae0b82af055b0c1730ee89e53e1aa7
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-01-19 06:14:03 +0900

    code update.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,72 @@
 #include <iostream>
+#include <string>
+#include <stack>
 using namespace std;
 
 int main()
 {
 	cin.tie(NULL);
+	cout.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	int a;
-	cin >> a;
+	char a, b;
+	string s;
+	stack<char> st;
+	while (true)
+	{
+		getline(cin, s);
+		if (s == ".") break;
 
-	cout << (long)(4 * a);
+		for (int i = 0; i < s.length(); i++)
+		{
+			a = s[i];
+
+			if (a == '(')
+			{
+				st.push(a);
+				b = a;
+			}
+			if (a == ')')
+			{
+				if (b == '(')
+				{
+					st.pop();
+					if (st.empty()) b = '?';
+					else b = st.top();
+				}
+				else
+				{
+					st.push(a);
+					b = a;
+				}
+
+			}
+			if (a == '[')
+			{
+				st.push(a);
+				b = a;
+			}
+			if (a == ']')
+			{
+				if (b == '[')
+				{
+					st.pop();
+					if (st.empty()) b = '?';
+					else b = st.top();
+				}
+				else
+				{
+					st.push(a);
+					b = a;
+				}
+			}
+		}
+
+		if (st.empty()) cout << "yes" << '\n';
+		else cout << "no" << '\n';
+
+		while (!st.empty()) st.pop();
+	}
 
 	return 0;
 }
