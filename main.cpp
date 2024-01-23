a3186684659a65d3ba332108c10ed5b2bb0081d2
yeosu623 <72304945+yeosu623@users.noreply.github.com>
2024-01-24 00:06:23 +0900

    code update.

--- main.cpp
+++ main.cpp
@@ -1,72 +1,9 @@
 #include <iostream>
-#include <string>
-#include <stack>
+#include <vector>
+#include <cmath>
 using namespace std;
 
 int main()
 {
-	cin.tie(NULL);
-	cout.tie(NULL);
-	ios::sync_with_stdio(false);
-
-	char a, b;
-	string s;
-	stack<char> st;
-	while (true)
-	{
-		getline(cin, s);
-		if (s == ".") break;
-
-		for (int i = 0; i < s.length(); i++)
-		{
-			a = s[i];
-
-			if (a == '(')
-			{
-				st.push(a);
-				b = a;
-			}
-			if (a == ')')
-			{
-				if (b == '(')
-				{
-					st.pop();
-					if (st.empty()) b = '?';
-					else b = st.top();
-				}
-				else
-				{
-					st.push(a);
-					b = a;
-				}
-
-			}
-			if (a == '[')
-			{
-				st.push(a);
-				b = a;
-			}
-			if (a == ']')
-			{
-				if (b == '[')
-				{
-					st.pop();
-					if (st.empty()) b = '?';
-					else b = st.top();
-				}
-				else
-				{
-					st.push(a);
-					b = a;
-				}
-			}
-		}
-
-		if (st.empty()) cout << "yes" << '\n';
-		else cout << "no" << '\n';
-
-		while (!st.empty()) st.pop();
-	}
-
-	return 0;
+	cout << (-3)/3;
 }
